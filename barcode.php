c0ef38f3b9e38aea167360095ef05ff387d6760c
Mohan9999 <36534476+Mohan9999@users.noreply.github.com>
2020-01-23 16:27:21 +0530

    Update barcode.php

--- barcode.php
+++ barcode.php
@@ -10,11 +10,11 @@
     <form method="GET" action="takebook.php">
         <div class="form-group">
             <label for="sbook">Scan Book</label>
-            <input type="text" name="book" class="form-control" placeholder="Scan Book or enter Book id" id="sbook">
+            <input type="text" name="book" class="form-control" placeholder="Scan Book or Enter Book ID" id="sbook">
         </div>
         <div class="form-group">
-            <label for="sid">Scan Student Id</label>
-            <input type="text" name="std" class="form-control" placeholder="Scan Student Id" id="sid">
+            <label for="sid">Scan People ID</label>
+            <input type="text" name="std" class="form-control" placeholder="Scan People ID" id="sid">
         </div>
         <input type="submit" class="btn btn-primary" name="submit">
     </form>
