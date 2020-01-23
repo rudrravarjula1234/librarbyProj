9f78c1a2087e4aaf27b6bd2966c7a3de6cf12b8e
Mohan9999 <36534476+Mohan9999@users.noreply.github.com>
2020-01-24 00:49:34 +0530

    1

--- addcopies.php
+++ addcopies.php
@@ -1,41 +1,51 @@
 <html>
-<head>
-<style>
-p.inline {display: inline-block;}
-span { font-size: 13px;}
-</style>
-<style type="text/css" media="print">
-    @page 
-    {
-        size: auto;   /* auto is the initial value */
-        margin: 0mm;  /* this affects the margin in the printer settings */
 
-    }
-</style>
+<head>
+    <style>
+        p.inline {
+            display: inline-block;
+        }
+
+        span {
+            font-size: 13px;
+        }
+    </style>
+    <style type="text/css" media="print">
+        @page {
+            size: auto;
+            /* auto is the initial value */
+            margin: 0mm;
+            /* this affects the margin in the printer settings */
+
+        }
+    </style>
 </head>
+
 <body onload="window.print();">
-	<div style="margin-left: 5%">
-    <?php
-    include "db.php";
-    include 'barCodelib.php';
-    $bid = $_POST['bid'];
-    $copies = $_POST['copy'];
-    $bookname = $_POST['bname'];
-    $uid = uniqid();
-    $array = array();
-    while($copies > 0){
-        $copyid = $bid . $uid . $copies;
-        $copies--;
-        $qu = mysqli_query($con,"INSERT INTO `booksdata` (`BookGuid`,`BookName`) values('$copyid','$bid')") or die(mysqli_error($con));
-        if($qu){
-            array_push($array,$copyid);
+    <div style="margin-left: 5%">
+        <?php
+        include "db.php";
+        include 'barCodelib.php';
+        $bid = $_POST['bid'];
+        $copies = $_POST['copy'];
+        $bookname = $_POST['bname'];
+        $uid = uniqid();
+        $array = array();
+        while ($copies > 0) {
+            $copyid = $bid . $uid . $copies;
+            $copies--;
+            $qu = mysqli_query($con, "INSERT INTO `booksdata` (`BookGuid`,`BookName`) values('$copyid','$bid')") or die(mysqli_error($con));
+            if ($qu) {
+                array_push($array, $copyid);
+            }
         }
-    }
-    
+
         foreach ($array as $key => $value) {
-            echo "<p class='inline'><span ><b>Item: $bookname</b></span>".bar128(stripcslashes($value))."</p>&nbsp&nbsp&nbsp&nbsp";
+            echo "<p class='inline'><span ><b>IDEAL INSTITUTE OF TECHNOLOGY</b></span>" . bar128(stripcslashes($value)) . "</p>&nbsp&nbsp&nbsp&nbsp
+            <p class='inline'><span ><b>IDEAL INSTITUTE OF TECHNOLOGY</b></span>" . bar128(stripcslashes($value)) . "</p>&nbsp&nbsp&nbsp&nbsp";
         }
-		?>
-	</div>
+        ?>
+    </div>
 </body>
+
 </html>
--- takebook.php
+++ takebook.php
@@ -1,36 +1,31 @@
 <?php
-    include 'db.php';
-    $book = $_GET['book'];
-    $std = $_GET['std'];
-    $bstate = mysqli_query($con,"SELECT `status` from `booksdata` where `booksdata`.`BookGuid` = '$book'");
-    $bstate = mysqli_fetch_array($bstate)[0];
-    $limit = mysqli_query($con,"SELECT CASE
+include 'db.php';
+$book = $_GET['book'];
+$std = $_GET['std'];
+$bstate = mysqli_query($con, "SELECT `status` from `booksdata` where `booksdata`.`BookGuid` = '$book'");
+$bstate = mysqli_fetch_array($bstate)[0];
+$limit = mysqli_query($con, "SELECT CASE
     WHEN `type` = 1 THEN `Books` < 3
     WHEN `type` = 2 THEN `Books` < 4
     END
-    FROM `stddataa` WHERE `stdid` = $std") or die(mysqli_error($con));
-    $limit = mysqli_fetch_array($limit)[0];
-    echo $bstate != 1;
-    echo $limit;
-    if(($book != null || $book != "")&&($std != null || $std != "") && $bstate != 1 && $limit){
-        mysqli_autocommit($con,FALSE);
-        $status = mysqli_query($con,"UPDATE `booksdata` SET `status` = true , `stdid` = '$std' WHERE `booksdata`.`BookGuid` = '$book' and `booksdata`.`status` = false") ; 
-        $tran = mysqli_query($con,"INSERT INTO `transaction` (`BookGiud`,`StdId`,`type`) VALUES('$book','$std',1)") ;
-        $std = mysqli_query($con,"UPDATE `stddataa` SET `Books` = `Books` + 1  WHERE `stdid` = '$std'") ;
-        if($status && $tran && $std){
-            mysqli_commit($con);
-            mysqli_autocommit($con,TRUE);
-            echo 1;
-        }
-        else{
-            mysqli_rollback($con);
-            mysqli_autocommit($con,TRUE);
-            echo "transaction failed";
-        }
-
-        
+    FROM `stddataa` WHERE `stdid` = '$std'") or die(mysqli_error($con));
+$limit = mysqli_fetch_array($limit)[0];
+echo $bstate != 1;
+echo $limit;
+if (($book != null || $book != "") && ($std != null || $std != "") && $bstate != 1 && $limit) {
+    mysqli_autocommit($con, FALSE);
+    $status = mysqli_query($con, "UPDATE `booksdata` SET `status` = true , `stdid` = '$std' WHERE `booksdata`.`BookGuid` = '$book' and `booksdata`.`status` = false");
+    $tran = mysqli_query($con, "INSERT INTO `transaction` (`BookGiud`,`StdId`,`type`) VALUES('$book','$std',1)");
+    $std = mysqli_query($con, "UPDATE `stddataa` SET `Books` = `Books` + 1  WHERE `stdid` = '$std'");
+    if ($status && $tran && $std) {
+        mysqli_commit($con);
+        mysqli_autocommit($con, TRUE);
+        echo 1;
+    } else {
+        mysqli_rollback($con);
+        mysqli_autocommit($con, TRUE);
+        echo "transaction failed";
     }
-    else{
-        echo "invalid data";
-    }
-?>
+} else {
+    echo "invalid data";
+}
